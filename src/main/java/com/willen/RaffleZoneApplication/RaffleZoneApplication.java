0f3da5fdf2735d96bd2ec630b1511d9e68800ef4
willen <willen.sesantos@gmail.com>
2023-07-28 10:49:17 -0300

    Primeiro commit

---
+++ src/main/java/com/willen/RaffleZoneApplication/RaffleZoneApplication.java
@@ -0,0 +1,13 @@
+package com.willen.RaffleZoneApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaffleZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaffleZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willen/RaffleZoneApplication/RaffleZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willen.RaffleZoneApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaffleZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
